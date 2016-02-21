c0e7e7e1ea0dea002d4f0cc40bc62eed02a28378
takayoshi.uchida <taum0715@gmail.com>
2016-02-22 00:36:31 +0900

    add FAB menu.

--- app/src/main/java/com/example/takayoshi/hatenareader/activities/HotentryItemActivity.java
+++ app/src/main/java/com/example/takayoshi/hatenareader/activities/HotentryItemActivity.java
@@ -3,7 +3,9 @@ package com.example.takayoshi.hatenareader.activities;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,16 +13,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.android.volley.toolbox.StringRequest;
 import com.example.takayoshi.hatenareader.R;
 
 import static com.example.takayoshi.hatenareader.utils.LogUtils.makeLogTag;
@@ -35,7 +36,9 @@ public class HotentryItemActivity extends AppCompatActivity {
 
     private WebView webView;
     private ProgressBar progressBar;
-    private FloatingActionButton fab;
+    private FloatingActionButton fab, fab_menu1, fab_menu2;
+    private boolean isFabOpen = false;
+    private Animation fab_open,fab_close;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,16 +76,43 @@ public class HotentryItemActivity extends AppCompatActivity {
         // FloatingActionButton の設定
         final CoordinatorLayout coordinatorLayout = (CoordinatorLayout)findViewById(R.id.coordinator_layout);
         fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab_menu1 = (FloatingActionButton) findViewById(R.id.fab_menu1);
+        fab_menu2 = (FloatingActionButton) findViewById(R.id.fab_menu2);
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                WebView showingView = ((WebView)((View)view.getParentForAccessibility()).findViewById(R.id.webview));
+                animateFAB();
+            }
+        });
+        fab_menu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WebView showingView = ((WebView)((View)v.getParentForAccessibility()).findViewById(R.id.webview));
+                // 単純にブラウザで表示させる
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(showingView.getUrl()));
+                startActivity(i);
+            }
+        });
+        fab_menu2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: はてなブックマーク追加をIntentで直コールする予定
+//                PackageManager pm = getPackageManager();
+//                Intent intent = pm.getLaunchIntentForPackage("com.hatena.android.bookmark");
+//                Intent intent = new Intent(Intent.ACTION_SEND);
+//                intent.setClassName("com.hatena.android.bookmark",
+//                                    "com.hatena.android.bookmark.PostActivity");
+//                intent.setFlags(0x3080001);
+//                startActivity(intent);
 
-                // TODO: (2016/2/20 記)ブックマークなどがまだできないので暫定でクリップボードへコピー
+                WebView showingView = ((WebView)((View)v.getParentForAccessibility()).findViewById(R.id.webview));
+                // 暫定でクリップボードへコピー
                 final ClipboardManager clipboard = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
                 final String bkClipboardText = (clipboard.hasPrimaryClip())
-                                                    ? clipboard.getPrimaryClip().getItemAt(0).getText().toString()
-                                                    : "";
+                        ? clipboard.getPrimaryClip().getItemAt(0).getText().toString()
+                        : "";
                 clipboard.setPrimaryClip(ClipData.newPlainText("SHOW_URL", showingView.getUrl()));
 
                 // Material Designガイドラインに則り、Snackbar 表示時にFABと被らないようにするため、CoordinatorLayoutを使用する
@@ -106,5 +136,25 @@ public class HotentryItemActivity extends AppCompatActivity {
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    public void animateFAB(){
+        if(isFabOpen){
+//            fab.startAnimation(rotate_backward);
+            fab_menu1.startAnimation(fab_close);
+            fab_menu2.startAnimation(fab_close);
+            fab_menu1.setClickable(false);
+            fab_menu2.setClickable(false);
+            isFabOpen = false;
+            Log.d("Raj", "close");
+        } else {
+//            fab.startAnimation(rotate_forward);
+            fab_menu1.startAnimation(fab_open);
+            fab_menu2.startAnimation(fab_open);
+            fab_menu1.setClickable(true);
+            fab_menu2.setClickable(true);
+            isFabOpen = true;
+            Log.d("Raj","open");
+        }
     }
 }
